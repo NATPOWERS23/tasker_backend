e207c38b5c5b0e2b748a0a6ae1747cbdb5e52377
Nataliia Balabukha <70195460+NATALIALOZER@users.noreply.github.com>
2023-12-05 19:30:42 +0100

    changed title task field validation

--- backend/validations/task.js
+++ backend/validations/task.js
@@ -1,7 +1,7 @@
 import { body } from 'express-validator';
 
 export const taskCreateValidation = [
-    body('title', 'Incorrect title format').isLength({ min: 2 }).isString(),
+    body('title', 'Incorrect title format').optional().isLength({ min: 2 }).isString(),
     body('content', 'Incorrect content format').optional().isString(),
     body('tags', 'Incorrect tags').optional().isString(),
     body('checked', 'Incorrect check value').optional().isBoolean(),
